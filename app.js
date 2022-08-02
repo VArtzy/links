67cc46d7b91923b645e9d85a859b1c7234155d89
VArtzy <farrelnikoson@gmail.com>
2022-08-02 21:38:55 +0700

    fix animation

--- app.js
+++ app.js
@@ -18,9 +18,28 @@ window.addEventListener("DOMContentLoaded", () => {
         cohesion: 5.0,
         quantity: 1.0,
     })
+    let count = 0
+    const links = document.querySelectorAll("li")
+    setInterval(() => {
+        if (count === 4) {
+            count = 0
+        } else {
+            if (count === 0) {
+                links[3].classList.remove("animate__swing")
+            } else {
+                links[count - 1].classList.remove("animate__swing")
+            }
+            links[count].classList.add("animate__swing")
+            count++
+        }
+    }, 5000)
     setTimeout(() => {
         const main = document.querySelector("main")
+        const footer = document.querySelector("footer")
         main.style.opacity = 1
         main.style.filter = "blur(0px)"
+        footer.style.opacity = 1
+        footer.style.filter = "blur(0px)"
+        footer.style.transform = "translateY(0)"
     }, 1000)
 })
